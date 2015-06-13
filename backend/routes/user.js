65ca3cf6a334420bb749e3e265cf64cec8e35b69
José Andrés García <jags9415@gmail.com>
2015-06-13 14:31:16 -0600

    get user

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -2,5 +2,13 @@ var parse = require('./../parse');
 
 exports.getUser = function(req, res, next) {
 	var email = req.param("email");
-	res.json({"response": email});
+	
+	parse.find('User', { "email": email }, function (err, response) {
+		if (!err) {
+			res.json(200, response);
+		}
+		else {
+			res.json(400, {"err": "Invalid email"});
+		}
+	});
 }
